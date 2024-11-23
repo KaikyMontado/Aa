9fdd682b5bd9398fbc2f655de63c63ffcd11f8c3
KaikyMontado <00001095437537sp@al.educacao.sp.gov.br>
2024-11-23 15:46:14 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,55 @@
+criaCartao(
+    'Lui', 
+    'Qual o nome do cachorro do Lui',
+      'Pólo'
+)
+
+criaCartao(
+    'Academia', 
+    'Qual o exercicio mais conhecido de peitoral',
+    'Supino reto'
+)
+
+criaCartao(
+    'sistema solar',
+    'Qual e o maior planeta do nosso sistema solar',
+    'Jupiter'
+)
+
+criaCartao(
+    'Sistema solar',
+    'Qual o satelite natural da Terra',
+    'Lua'
+)                       
+
+criaCartao(
+    'Sistema solar',
+    'Qual e o nome da primeira missao tripulada a Lua',
+    'Apollo 11'
+)
+
+criaCartao(
+    'Literatura',
+    'Quem escreveu "Dom Quixote"?',
+    'Miguel de Cervantes'
+
+)
+
+criaCartao(
+    'Esportes',
+    'Quantos jogadores cada time tem em campo no futebol?',
+    '11 jogadores'
+
+)
+
+criaCartao(
+    'Ciências',
+    'Qual é o elemento químico representado pela sigla O?',
+    'Oxigênio'
+)
+
+criaCartao(
+    'Cinema',
+    'Qual filme ganhou o Oscar de Melhor Filme em 1994?',
+    'Forrest Gump'
+)
